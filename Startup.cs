80fb4fd1c9d0e02adb40836c81e28a79ba3fa3b1
artem-reroot <triple8six@gmail.com>
2020-09-16 20:17:03 -0400

    fixed

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace dotnet_ai_test_one
 {
     public class Startup
@@ -31,13 +32,6 @@ namespace dotnet_ai_test_one
 
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
-
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
