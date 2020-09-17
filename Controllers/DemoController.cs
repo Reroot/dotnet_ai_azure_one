55888e333ff95fe6343d8b7b09b46fd915a958ab
artem-reroot <triple8six@gmail.com>
2020-09-17 18:37:01 -0400

    p

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using demo.Models;
+//using demo.Models;
 using System.Net.Http;
 
 namespace demo.Controllers
